cd514c80bdf8ea6ba65dd85006b66f694548dd94
David Basden <david.b@kasada.io>
2017-12-04 12:29:25 +1100

    Pass most HTTP headers back and forth, ignoring connection control and transport encoding

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ AWS.CredentialProviderChain.defaultProviders = [
   () => { return new AWS.EC2MetadataCredentials(); }
 ];
 
-var execute = function(endpoint, region, path, method, body) {
+var execute = function(endpoint, region, path, headers, method, body) {
   return new Promise((resolve, reject) => {
     var req = new AWS.HttpRequest(endpoint);
 
@@ -27,25 +27,27 @@ var execute = function(endpoint, region, path, method, body) {
     req.method = method || 'GET';
     req.path = path;
     req.region = region;
-
-    if(body) {
-      if(typeof body === "object") {
-        req.body = JSON.stringify(body);
-      } else {
-        req.body = body;
-      }
-    }
-
-    // Sense likes send GET request with body, which aws-sdk doesn't really allow. Translate to POST instead.
-    if(req.body && req.method == 'GET') {
-      req.method = 'POST';
-    }
+    req.body = body;
 
     req.headers['presigned-expires'] = false;
     req.headers.Host = endpoint.host;
 
     var signer = new AWS.Signers.V4(req, 'es');
     signer.addAuthorization(creds, new Date());
+
+    // Now we have signed the "headers", we add extra headers passing
+    // from the browser.  We must strip any connection control, transport encoding
+    // incorrect Origin headers, and make sure we don't change the Host header from
+    // the one used for signing
+    var entries = Object.entries(headers)
+    for (var i=0, len=entries.length; i<len; i++) {
+            if (entries[i][0] != "host" && 
+                entries[i][0] != "accept-encoding" &&
+                entries[i][0] != "connection" &&
+                entries[i][0] != "origin") {
+                req.headers[entries[i][0]] = entries[i][1]
+            }
+    }
 
     var send = new AWS.NodeHttpClient();
     send.handleRequest(req, null, (httpResp) => {
@@ -56,6 +58,7 @@ var execute = function(endpoint, region, path, method, body) {
       httpResp.on('end', (chunk) => {
         resolve({
           statusCode: httpResp.statusCode,
+          headers: httpResp.headers,
           body: body
         });
       });
@@ -91,10 +94,21 @@ var requestHandler = function(request, response) {
     var buf = Buffer.concat(body).toString();
 
     co(function*(){
-        return yield execute(context.endpoint, context.region, request.url, request.method, buf);
+        return yield execute(context.endpoint, context.region, request.url, request.headers, request.method, buf);
       })
       .then(resp => {
-        response.writeHead(resp.statusCode, { 'Content-Type': 'application/json' });
+        // We need to pass through the response headers from the origin
+        // back to the UA, but strip any connection control and content encoding
+        // headers
+        headers = {}
+        entries = Object.entries(resp.headers)
+        for (var i=0,  nent = entries.length; i<nent; i++) {
+                k = entries[i][0]; v=entries[i][1]
+                if (k != undefined && k != "connection" && k != "content-encoding") {
+                        headers[k] = v
+                }
+        }
+        response.writeHead(resp.statusCode, headers);
         response.end(resp.body);
       })
       .catch(err => {
